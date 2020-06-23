368107a3cc72d3551ef905016ae76a7437fc25bc
Mahmudur Rahman <mahmud.est@gmail.com>
2020-06-23 23:49:28 +0600

    Add files via upload

---
+++ precache-manifest.ef1e1b40f3927a10dc962fdb5913617d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1497523785ebc667349b6fb32dadfc75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c279a544468a6826dec1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "38e86fff58a5eabe7ea7",
+    "url": "/static/css/main.a58f5e81.chunk.css"
+  },
+  {
+    "revision": "c279a544468a6826dec1",
+    "url": "/static/js/2.729375fa.chunk.js"
+  },
+  {
+    "revision": "38e86fff58a5eabe7ea7",
+    "url": "/static/js/main.a358d137.chunk.js"
+  },
+  {
+    "revision": "a0f6f6f7d3dafe836686",
+    "url": "/static/js/runtime-main.0b28f6c4.js"
+  }
+]);
